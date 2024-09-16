32cf535893e5533b97efaf9ce9c15d37d84c5a58
Maroš Hanzel <maros.hanzel@icloud.com>
2024-09-16 22:07:06 +0200

    feat: implement team uniqueness assumption validation

--- src/main/java/com/sportradar/interview/repository/MatchRepository.java
+++ src/main/java/com/sportradar/interview/repository/MatchRepository.java
@@ -14,6 +14,18 @@ public class MatchRepository {
     private final Supplier<Integer> idSupplier = idCounter::getAndIncrement;
 
     public Match save(String homeTeam, String awayTeam) {
+        matches.values().stream()
+            .filter(match -> teamAlreadyStored(match, homeTeam, awayTeam))
+            .findFirst()
+            .ifPresent(match -> {
+                throw new IllegalStateException(String.format(
+                    "At least one of the teams %s/%s already stored in match %s",
+                    homeTeam,
+                    awayTeam,
+                    match
+                ));
+            });
+
         Match match = new Match.Builder()
             .id(idSupplier.get())
             .homeTeam(homeTeam)
@@ -21,6 +33,13 @@ public class MatchRepository {
             .build();
         matches.put(match.id(), match);
         return match;
+    }
+
+    private boolean teamAlreadyStored(Match match, String homeTeam, String awayTeam) {
+        return match.homeTeam().name().equals(homeTeam)
+            || match.homeTeam().name().equals(awayTeam)
+            || match.awayTeam().name().equals(homeTeam)
+            || match.awayTeam().name().equals(awayTeam);
     }
 
 }
